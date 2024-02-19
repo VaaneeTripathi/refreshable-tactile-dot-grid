302c629ad87ec227148c4772e4b1fdd0fb595727
Vaanee Tripathi <124565270+VaaneeTripathi@users.noreply.github.com>
2024-02-19 20:35:39 +0530

    Add code for video to image conversion

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 # Program to convert video stream to images and process them 
 import cv2 as cv 
+import contour_detection
 
-# Take a videostream, output images 
-video = cv.VideoCapture('sample.mp4')
+def main(filename):
 
-# TODO: make contour_detection a function
-# TODO: read from file - get fps - get the frame - contour_detection - return drawed contours 
+    # Take a videostream, output images 
+    video = cv.VideoCapture(filename)
+    
+    # Get the number of frames
+    no_of_frames = video.get(cv.CAP_PROP_FRAME_COUNT)
+
+    for frame_id in range(no_of_frames):
+   
+        # Extract the frame from the video
+        video.set(cv.CAP_PROP_POS_FRAME, frame_id)
+        ret, frame = video.read()
+        contour_detection(frame)
+
 
